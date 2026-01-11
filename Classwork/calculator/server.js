7d9ca14b654f4dad343adcdbab8dfa2b556c89c6
Kishan Raghav <kishanraghav1674@gmail.com>
2026-01-11 15:33:14 +0530

    Added server.js to use calculator methods

---
+++ Classwork/calculator/server.js
@@ -0,0 +1,5 @@
+const calc = require('./calculator');
+console.log("Add:", calc.add(5, 3));
+console.log("Subtract:", calc.subtract(5, 3));
+console.log("Multiply:", calc.multiply(5, 3));
+console.log("Divide:", calc.divide(5, 3));
