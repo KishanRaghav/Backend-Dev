8c9103b96ede994eacba6f77d2be7ea3f8afeef5
KishanRaghav <kishanraghav1674@gmail.com>
2026-02-02 14:45:36 +0530

    Delete Classwork/Classwork directory

--- Classwork/Classwork/NodeJS/exercise2-custom-module/app.js
+++
@@ -1,7 +0,0 @@
-const utils = require('./stringUtils');
-
-const text = "nodejs";
-
-console.log(utils.capitalize(text));
-console.log(utils.reverseString(text));
-console.log(utils.countVowels(text));
